9b00924138999f88df05791da12ae508ac05779e
Ieva <ieva.jaksaityte@gmail.com>
2017-12-04 18:34:28 +0200

    created car class

---
+++ automobiliai su db/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace automobiliai_su_db
+{
+    class Car
+    {
+        public string Make { get; private set; }
+        public string Model { get; private set; }
+        public int Year { get; private set; }
+        public int Mileage { get; private set; }
+
+        public Car(string make, string model, int year, int mileage)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Mileage = mileage;
+        }
+    }
+}
